6cc2788fd3a6d041d940b18cfff4e5ce2af4fb85
AmirAhmed0s <163478540+AmirAhmed0s@users.noreply.github.com>
2024-05-27 13:47:22 +0300

    ch

--- attendance/attendance/doctype/attendance_calculation/test_attendance_calculation.py
+++ attendance/attendance/doctype/attendance_calculation/test_attendance_calculation.py
@@ -1,1 +1,9 @@
+# Copyright (c) 2021, Peter Maged and Contributors
+# See license.txt
 
+# import frappe
+import unittest
+https://github.com/AmirAhmed0s/attendance_hr/blob/master/attendance/attendance/doctype/attendance_calculation/test_attendance_calculation.py
+class TestAttendanceCalculation(unittest.TestCase):
+	pass
+test_attendance_calculation.py
